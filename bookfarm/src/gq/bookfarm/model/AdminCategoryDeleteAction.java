977afc3fbafa1953ddccd8b40c559e7275d4798c
SamSohn73 <samsohn73@gmail.com>
2018-01-10 15:18:39 +0900

    commit for the missing one

--- bookfarm/src/gq/bookfarm/model/AdminCategoryDeleteAction.java
+++ bookfarm/src/gq/bookfarm/model/AdminCategoryDeleteAction.java
@@ -2,12 +2,15 @@ package gq.bookfarm.model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
 import gq.bookfarm.action.Action;
 import gq.bookfarm.action.ActionForward;
+import gq.bookfarm.dao.AdminDAO;
 import gq.bookfarm.dao.CategoryDAO;
+import gq.bookfarm.vo.AdminVO;
 
 public class AdminCategoryDeleteAction implements Action {
 	
@@ -20,10 +23,19 @@ public class AdminCategoryDeleteAction implements Action {
 		log.debug("AdminCategoryDeleteAction Constructor. Destination path = " + path);
 	}
 	@Override
-	public ActionForward execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse res) throws Exception
+	{
 		log.debug("AdminCategoryDeleteAction execute Start.");
+		
+		HttpSession	session		= req.getSession();
+		AdminVO		adminVO		= (AdminVO) session.getAttribute("adminVO");
+		AdminDAO	adminDAO	= new AdminDAO();
+		if (adminDAO.isAdmin(adminVO) == null) {
+			log.info("AdminCategoryDeleteAction execute Authorization Fail!!!!!!!!!!!!!!!!");
+			path="error.jsp";
+		}
+		
 		String current_page = req.getParameter("page");
-		log.debug("AdminCategoryDeleteAction execute Page=" + current_page);
 		int idx=Integer.parseInt(req.getParameter("idx"));
 		
 		CategoryDAO dao	= new CategoryDAO();
